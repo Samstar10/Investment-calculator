3235024366645a3c130958fa6655787e0d5084cc
Samuel Muli <samuelmuli95@gmail.com>
2024-03-27 13:09:04 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/investment-calculator.png'
+
+export default function Header(){
+    return (
+        <header>
+            <img src={logo} alt="Logo showing a money bag" />
+        </header>
+    )
+}
