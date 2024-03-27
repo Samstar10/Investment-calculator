3a6fbf413c0b4d31d96eddb5561b0e1d57fb9367
Samuel Muli <samuelmuli95@gmail.com>
2024-03-27 13:26:49 +0300

    Create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import logo from '../assets/investment-calculator.png'
+import logo from '../assets/investment-calculator-logo.png'
 
 export default function Header(){
     return (
-        <header>
+        <header id='header'>
             <img src={logo} alt="Logo showing a money bag" />
+            <h1>Investment Calculator</h1>
         </header>
     )
 }
